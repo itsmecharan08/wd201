37a077b8fbec7dff1a21f4999cd87d5c28d56dd5
Sai Charan <114416673+itsmecharan08@users.noreply.github.com>
2023-12-20 21:28:27 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -20,7 +20,7 @@ describe("TodoList Test Suite", () => {
 
   test("Should mark a todo as completed after its completion", () => {
     todo.add({
-      title: "Incomplete Todo",
+      title: "Incompleted Todo",
       completed: false,
       dueDate: "2023-12-31",
     });
@@ -34,7 +34,7 @@ describe("TodoList Test Suite", () => {
     
 
     todo.add({
-      title: "Overdue Todo",
+      title: "Overdued Todo",
       completed: false,
       dueDate: "2023-01-01",
     });

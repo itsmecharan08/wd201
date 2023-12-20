53d46347ed70c34ebb38fdabe89363c2aea35acd
Sai Charan <114416673+itsmecharan08@users.noreply.github.com>
2023-12-20 19:03:30 +0530

    Update completeTodo.js

--- todo-cli/completeTodo.js
+++ todo-cli/completeTodo.js
@@ -11,10 +11,10 @@ const markAsComplete = async (id) => {
 (async () => {
   const { id } = argv;
   if(!id) {
-    throw new Error("Need to pass an id");
+    throw new Error("Need to pass a id");
   }
   if(!Number.isInteger(id)) {
-    throw new Error("The id needs to be an integer")
+    throw new Error("The id needs to be a integer")
   }
   await markAsComplete(id);
   await db.Todo.showList();
